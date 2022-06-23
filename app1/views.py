6a6e538432c2b6282a3457a8a4789681a75496fb
Rob Monday <nospam4rob@gmail.com>
2022-06-23 18:51:02 -0400

    put some basic code in place based on codecademy project

--- app1/views.py
+++ app1/views.py
@@ -1,4 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+# from .models import State, Attraction
+# from .forms import StateCreateForm, AttractionCreateForm
+# from django.views.generic.edit import CreateView
 
-# Create your views here..
+def home(request):
+  context = {"key" : "value"}
+  return render(request, 'app1/base.html', context)
 
+# def details(request, statename):
+#   attractions = Attraction.objects.all()
+#   context = {"attractions" : attractions, "statename" : statename.replace("-", " ")}
+#   return render(request, 'tourist_attractions/details.html', context)
+
+# class StateCreate(CreateView):
+#   model = State
+#   form_class = StateCreateForm
+#   template_name = "tourist_attractions/state_create_form.html"
+
+# class AttractionCreate(CreateView):
+#   model = Attraction
+#   form_class = AttractionCreateForm
+#   template_name = "tourist_attractions/attraction_create_form.html"
