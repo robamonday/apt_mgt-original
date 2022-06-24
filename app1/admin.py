ea7402923a3040b63e2bf3b708cbc126870fe7ab
Rob Monday <nospam4rob@gmail.com>
2022-06-24 13:40:47 -0400

    update to enable editing models from admin interface...also adding changes from units.html

--- app1/admin.py
+++ app1/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Unit, Lease, LedgerEntry
+ 
+admin.site.register(Unit)
+admin.site.register(Lease)
+admin.site.register(LedgerEntry)
